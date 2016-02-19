d179d1d80cc18074dd7d443e0c14bf5ee0cfd54f
username-CharlieH <cahenze@hotmail.com>
2016-02-19 10:05:11 -0800

    For Ex1; merge child class into program; Add code for  walkthrough portions to shell.

--- Ex1-Connection-And-Hierarchy-Basics-Sln/Program.cs
+++ Ex1-Connection-And-Hierarchy-Basics-Sln/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using External;
+using OSIsoft.AF;
+using OSIsoft.AF.Asset;
+using OSIsoft.AF.UnitsOfMeasure;
 
 namespace Ex1_Connection_And_Hierarchy_Basics_Sln
 {
@@ -7,16 +9,126 @@ namespace Ex1_Connection_And_Hierarchy_Basics_Sln
     {
         static void Main(string[] args)
         {
-            AFPrinter afPrinter = new AFPrinter("PISRV01", "Magical Power Company");
-            afPrinter.PrintRootElements();
-            afPrinter.PrintElementTemplates();
-            afPrinter.PrintAttributeTemplates("MeterAdvanced");
-            afPrinter.PrintEnergyUOMs();
-            afPrinter.PrintEnumerationSets();
-            afPrinter.PrintCategories();
+            AFDatabase database = GetDatabase(Environment.MachineName, "Magical Power Company");
+            PrintRootElements(database);
+            PrintElementTemplates(database);
+            PrintAttributeTemplates(database, "MeterAdvanced");
+            PrintEnergyUOMs(database.PISystem);
+            PrintEnumerationSets(database);
+            PrintCategories(database);
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
+        }
+
+        static AFDatabase GetDatabase(string servername, string databasename)
+        {
+            PISystems piafsystems = new PISystems();
+            PISystem system = piafsystems[servername];
+            if (system != null && system.Databases.Contains(databasename))
+            {
+                Console.WriteLine("Found '{0}' with '{1}' databases", system.Name, system.Databases.Count);
+                return system.Databases[databasename];
+            }
+            else
+                return null;
+        }
+
+        static void PrintRootElements(AFDatabase database)
+        {
+            Console.WriteLine("Print Root Elements: {0}", database.Elements.Count);
+            foreach (AFElement element in database.Elements)
+            {
+                Console.WriteLine("  {0}", element.Name);
+            }
+
+            Console.WriteLine();
+        }
+
+        static void PrintElementTemplates(AFDatabase database)
+        {
+            Console.WriteLine("Print Element Templates");
+            AFNamedCollectionList<AFElementTemplate> elemTemplates = database.ElementTemplates.FilterBy(typeof(AFElement));
+            foreach (AFElementTemplate elemTemp in elemTemplates)
+            {
+                // Note: An alternative and faster approach is to use eTemp.CategoriesString
+                string[] categories = new string[elemTemp.Categories.Count];
+                int i = 0;
+                foreach (AFCategory category in elemTemp.Categories)
+                {
+                    categories[i++] = category.Name;
+                }
+
+                string categoriesString = string.Join(",", categories);
+                Console.WriteLine("Name: {0}, Categories: {1}", elemTemp.Name, elemTemp.CategoriesString);
+            }
+
+            Console.WriteLine();
+        }
+
+        static void PrintAttributeTemplates(AFDatabase database, string elemTempName)
+        {
+            Console.WriteLine("Print Attribute Templates for Element Template: {0}", elemTempName);
+            AFElementTemplate elemTemp = database.ElementTemplates[elemTempName];
+            foreach (AFAttributeTemplate attrTemp in elemTemp.AttributeTemplates)
+            {
+                string drName = attrTemp.DataReferencePlugIn == null ? "None" : attrTemp.DataReferencePlugIn.Name;
+                Console.WriteLine("Name: {0}, DRPlugin: {1}", attrTemp.Name, drName);
+            }
+
+            Console.WriteLine();
+        }
+
+        static void PrintEnergyUOMs(PISystem system)
+        {
+            Console.WriteLine("Print Energy UOMs");
+            UOMClass uomClass = system.UOMDatabase.UOMClasses["Energy"];
+            foreach (UOM uom in uomClass.UOMs)
+            {
+                Console.WriteLine("UOM: {0}, Abbreviation: {1}", uom.Name, uom.Abbreviation);
+            }
+
+            Console.WriteLine();
+        }
+
+        static void PrintEnumerationSets(AFDatabase database)
+        {
+            Console.WriteLine("Print Enumeration Sets\n");
+            AFEnumerationSets enumSets = database.EnumerationSets;
+            foreach (AFEnumerationSet enumSet in enumSets)
+            {
+                Console.WriteLine(enumSet.Name);
+                foreach (AFEnumerationValue state in enumSet)
+                {
+                    int stateValue = state.Value;
+                    string stateName = state.Name;
+                    Console.WriteLine("{0} - {1}", stateValue, stateName);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintCategories(AFDatabase database)
+        {
+            Console.WriteLine("Print Categories\n");
+            AFCategories elemCategories = database.ElementCategories;
+            AFCategories attrCategories = database.AttributeCategories;
+
+            Console.WriteLine("Element Categories");
+            foreach (AFCategory category in elemCategories)
+            {
+                Console.WriteLine(category.Name);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Attribute Categories");
+            foreach (AFCategory category in attrCategories)
+            {
+                Console.WriteLine(category.Name);
+            }
+
+            Console.WriteLine();
         }
     }
 }
--- Ex1-Connection-And-Hierarchy-Basics/Program.cs
+++ Ex1-Connection-And-Hierarchy-Basics/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using External;
+using OSIsoft.AF;
+using OSIsoft.AF.Asset;
+using OSIsoft.AF.UnitsOfMeasure;
 
 namespace Ex1_Connection_And_Hierarchy_Basics
 {
@@ -7,17 +9,65 @@ namespace Ex1_Connection_And_Hierarchy_Basics
     {
         static void Main(string[] args)
         {
-            // Uncomment the lines below in order to test.
-            // AFPrinter afPrinter = new AFPrinter("PISRV01", "Magical Power Company");
-            // afPrinter.PrintRootElements();
-            // afPrinter.PrintElementTemplates();
-            // afPrinter.PrintAttributeTemplates("MeterAdvanced");
-            // afPrinter.PrintEnergyUOMs();
-            // afPrinter.PrintEnumerationSets();
-            // afPrinter.PrintCategories();
+            AFDatabase database = GetDatabase(Environment.MachineName, "Magical Power Company");
+            PrintRootElements(database);
+            PrintElementTemplates(database);
+            PrintAttributeTemplates(database, "MeterAdvanced");
+            PrintEnergyUOMs(database.PISystem);
+            PrintEnumerationSets(database);
+            PrintCategories(database);
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
+        }
+
+        static AFDatabase GetDatabase(string servername, string databasename)
+        {
+            PISystems piafsystems = new PISystems();
+            PISystem system = piafsystems[servername];
+            if (system != null && system.Databases.Contains(databasename))
+            {
+                Console.WriteLine("Found '{0}' with '{1}' databases", system.Name, system.Databases.Count);
+                return system.Databases[databasename];
+            }
+            else
+                return null;
+        }
+
+        static void PrintRootElements(AFDatabase database)
+        {
+            Console.WriteLine("Print Root Elements: {0}", database.Elements.Count);
+            foreach (AFElement element in database.Elements)
+            {
+                Console.WriteLine("  {0}", element.Name);
+            }
+
+            Console.WriteLine();
+        }
+
+        static void PrintElementTemplates(AFDatabase database)
+        {
+            // Your code here
+        }
+
+        static void PrintAttributeTemplates(AFDatabase database, string elemTempName)
+        {
+            // Your code here
+        }
+
+        static void PrintEnergyUOMs(PISystem system)
+        {
+            // Your code here
+        }
+
+        static void PrintEnumerationSets(AFDatabase database)
+        {
+            // Your code here
+        }
+
+        static void PrintCategories(AFDatabase database)
+        {
+            // Your code here
         }
     }
 }
